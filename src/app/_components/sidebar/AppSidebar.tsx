bd58de95b86d6ccf8d18fad71982015a5adb3228
Binaya <sbinayaraj@gmail.com>
2025-10-27 12:49:44 +0200

    app sidebar logic completed

--- src/app/_components/sidebar/AppSidebar.tsx
+++ src/app/_components/sidebar/AppSidebar.tsx
@@ -18,8 +18,17 @@ import {
   CheckSquare,
   Briefcase,
   FileText,
+  LogOut,
+  BriefcaseBusiness,
 } from "lucide-react"
 import { SidebarItem } from "./SidebarItem";
+import { useSelector } from "react-redux";
+import { RootState } from "@/app/redux/store";
+import { useRouter, useSearchParams } from "next/navigation";
+import EditButton from "../structures/EditButton";
+import { Button } from "@/components/ui/button";
+import { useLogout } from "@/hooks/useLogout";
+import ProfileIcon from "../landingPage/UserDisplay/ProfileIcon";
 const sidebarItems = [
   { label: "Salary", icon: Wallet, href: "/salary" },
   { label: "Roadmap", icon: Route, href: "/roadmap" },
@@ -28,6 +37,11 @@ const sidebarItems = [
 const AppSidebar = () => {
   const { state, isMobile } = useSidebar()
   const showText = state === "expanded" || isMobile
+  const user = useSelector((state:RootState)=>state.user.user)
+  const searchParams = useSearchParams()
+  const jobTitle = searchParams.get('jobtitle')
+  const router = useRouter()
+  const LogOut = useLogout()
   return (
     <Sidebar collapsible="icon">
       <SidebarHeader>
@@ -42,7 +56,20 @@ const AppSidebar = () => {
           </ul>
         </SidebarGroup>
       </SidebarContent>
-      <SidebarFooter />
+      <SidebarFooter>
+        <div className="sidebarFooterHeader flex gap-2">
+          <h2 className="primaryPatagraph capitalize flex gap-2">
+            <BriefcaseBusiness/>
+            {jobTitle}
+            </h2>
+        {user?.jobTitles && user?.jobTitles?.length>0 && <EditButton onClick={()=>router.push('/profile')}/>}
+        </div>
+        <div className="sidebarProfile">
+        <p className="primaryParagraph">{user?.fullName || "Guest User"}</p>
+        </div>
+        {user? <Button variant={'destructive'} onClick={()=>LogOut.mutate}>Logout</Button>:
+        <Button variant={'secondary'} onClick={()=>router.push('/register')}> Register</Button>}
+      </SidebarFooter>
     </Sidebar>
   )
 }
